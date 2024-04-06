95e008b9ce4fc33289453ed0a0f7c717f3610a0e
Aryan D <aryan.d.dalal@gmail.com>
2024-04-07 04:58:59 +0530

    streamlit

---
+++ src/routes/PLFood.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let src;
+    export let width = "100%";
+    export let height = "500px";
+  </script>
+  
+  <iframe
+    {src}
+    {width}
+    {height}
+    frameborder="0"
+    allowfullscreen
+    title="Streamlit App"
+  />
+  
