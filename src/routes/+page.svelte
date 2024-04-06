6c13b772da2e7671ac4a35a61422bdb3bef48d50
Aryan D <aryan.d.dalal@gmail.com>
2024-04-06 12:13:02 +0530

    added navbar and ilovetrees

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,22 @@
+<script>
+    import NavbarButton from "./NavbarButton.svelte";
 
+</script>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
 <style>
     :global(h1,div){
-        font-family: sans-serif, Geneva, Tahoma, sans-serif;
+        font-family: "Poppins", sans-serif;
     }
 .title{
     max-width: 100%;
     text-align: center;
     font-size: 70px;
+    font-weight: 200;
 }
 </style>
-
+<NavbarButton link={'ilovetrees'} text={'amogus'} />
 <h1 class="title">The Bangaluru Upliftment Project</h1>
---
+++ src/routes/NavbarButton.svelte
@@ -0,0 +1,31 @@
+<script>
+    export let link = "";
+    export let text = "";
+</script>
+
+<a href={link}>
+    {text}
+</a>
+
+<style>
+    :root {
+        --navbtn: #8a2be2 ;
+        --txtwhite: #ffffff ;
+        --txtblack: #000000;
+    }
+    a {
+        padding: 10px;
+        border-radius: 15px;
+        background-color: rgba(0, 0, 0, 0);
+        border-color: var(--navbtn);
+        border-width: 2px;
+        border-style: solid;
+        text-decoration: none;
+        color: var(--txtblack);
+    }
+
+    a:hover {
+        background-color: var(--navbtn);
+        color:var(--txtwhite);
+    }
+</style>
